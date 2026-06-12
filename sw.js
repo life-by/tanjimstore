10cfb4a181c8909cb9bb43948946b363b6375102
Tangim <tangimislam014@gmail.com>
2026-06-12 19:01:19 +0600

    Update service worker to use new cache strategy

--- sw.js
+++ sw.js
@@ -1,47 +1,48 @@
+const CACHE_NAME = 'tanjim-store-v2';
 
-const CACHE_NAME = 'tanjimstore-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+// এখানে আপনার ওয়েবসাইটের জরুরি ফাইলগুলোর নাম দেওয়া হলো
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './data.json'
 ];
 
-// Install SW
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Fetch data
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-// Update SW
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.open(CACHE_NAME).then((cache) => {
+      // যদি কোনো ফাইল খুঁজে না পায়, তবুও যেন ক্র্যাশ না করে সেই ব্যবস্থা
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+        ASSETS_TO_CACHE.map(url => {
+          return cache.add(url).catch(err => console.log('এই ফাইলটি ক্যাশ করা যায়নি:', url));
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
       );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // অফলাইনে থাকলে ক্যাশ থেকে দেখাবে, অনলাইনে থাকলে ইন্টারনেট থেকে আনবে
+      return response || fetch(event.request).catch(() => {
+        console.log('অফলাইন মোড অ্যাক্টিভ');
+      });
     })
   );
 });
